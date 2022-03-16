3b930013419d9dda7c1dbba1bab20a58c2d46cb9
victor.geodata <victor.geodata@gmail.com>
2022-03-16 08:31:02 -0300

    exercise title case

--- src/app/pipetext.pipe.ts
+++ src/app/pipetext.pipe.ts
@@ -8,9 +8,31 @@ export class PipetextPipe implements PipeTransform {
     if(!value){
       return null;
     }
-    let firstLetter = value.substring(0, 1).toUpperCase();
-    let otherLetters = value.substring(1).toLowerCase();
-    return value.substring(0, 1).toUpperCase() + value.substring(1).toLowerCase();
 
+
+
+    let words = value.split(' ');
+    for(var i = 0; i < words.length; i++){
+      if( i> 0 && this.isPreposition(words[i].toLowerCase())){
+        words[i] = words[i].toLowerCase();
+      } else {
+        words[i] = this.toTitleCase(words[i]);
+      }
+    }
+    return words.join(' ');
   }
+
+  private toTitleCase(word: string):string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+  private isPreposition(word: string): boolean{
+    let preposition = [
+      "of",
+      "the"
+    ];
+
+    return preposition.includes(word.toLowerCase(),)
+  }
+
 }
